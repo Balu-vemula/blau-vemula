a62665bbca2d7e3aa7eb3758750fd19200f24c20
Balu-vemula <balaraju.vemula@zemosolabs.com>
2023-10-05 03:53:16 -0500

    added pr raise

--- ci-Scripts/cypress.sh
+++ ci-Scripts/cypress.sh
@@ -3,9 +3,8 @@
 echo "running tests"
 echo $TAGS
 echo $env
-echo $url
-
+echo $APP_URL
 
 npm install
 npx cypress install
-npx cypress run --config-file cypress.$env.config.js --env APP_URL=$url --env TAGS=$TAGS
+npx cypress run --config-file cypress.$env.config.js --env TAGS=$TAGS --env APP_URL=$APP_URL 
